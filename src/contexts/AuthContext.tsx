6e0cb1f6ab6adc8c738126f992ad22a9871541e0
biancaraiiane <bianca.raiane18@gmail.com>
2023-04-12 21:16:58 -0300

    projeto em andamento

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,68 @@
+import {createContext, ReactNode, useState} from 'react';
+
+//import {api} from '../services/apiClient';
+
+import { destroyCookie } from 'nookies';
+import Router from 'next/router';
+import axios, { AxiosInstance } from 'axios';
+
+const api = setupAPIClient();
+
+
+type AuthContextData = {
+    user: UserProps;
+    isAuthenticated: boolean;
+    signIn: (credentials: SignInProps) => Promise<void>;
+    signOut: (ctx?: any) => AxiosInstance
+    
+}
+
+type UserProps = {
+    id: string;
+    name: string;
+    email: string;
+}
+
+type SignInProps = {
+    email: string;
+    password: string;
+}
+
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut(){
+    try{
+        destroyCookie(undefined, '@nextAuth.token')
+        Router.push('/')
+    }catch{
+        console.log('erro ao deslogar')
+    }
+}
+
+export function AuthProvider({children}: AuthProviderProps){
+    const [user, setUser]= useState<UserProps>()
+    const isAuthenticated = !!user;
+
+    async function signIn({email, password}: SignInProps){
+        try{
+            const response = await api.post('/session', {
+                email,
+            })
+        }catch{
+
+        }
+    }
+    return(
+        <AuthContext.Provider value={{user, isAuthenticated, signIn, signOut }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+function setupAPIClient() {
+    throw new Error('Function not implemented.');
+}
