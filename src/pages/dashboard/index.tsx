63ea47fba188fda35f9bcf89265c5615910649d0
biancaraiiane <bianca.raiane18@gmail.com>
2023-04-27 22:32:44 -0300

    finalizando o projeto pizzaria web

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -72,6 +72,26 @@ export default function Dashboard({orders}: HomeProps){
 
     }
 
+    async function handleFinishItem(id: string){
+        const apiClient = setupAPIClient();
+        await apiClient.put('/order/finish', {
+            order_id: id,
+        })
+
+        const response = await apiClient.get('/orders');
+
+        setOrderList(response.data);
+        setModalVisible(false);
+    }
+
+    async function handleRefreshOrders(){
+        const apiClient = setupAPIClient();
+        
+        const response = await apiClient.get('/orders');
+
+        setOrderList(response.data);
+    }
+
     Modal.setAppElement('#__next');
 
     return(
@@ -87,12 +107,18 @@ export default function Dashboard({orders}: HomeProps){
                 <div className={styles.containerHeader}>
                     <h1>Últimos pedidos</h1>
 
-                    <button>
+                    <button onClick={handleRefreshOrders}>
                         <FiRefreshCcw size={25} color= "#3fffa3"/>
                     </button>
                 </div>
 
                 <article className={styles.listOrders}>
+
+                    {orderList.length === 0 && (
+                        <span className={styles.emptyList}>
+                            Nenhum pedido aberto foi encontrado.....
+                        </span>
+                    )}
 
                     {orderList.map(item => (
                          <section key={item.id} className={styles.orderItem}>
@@ -108,7 +134,12 @@ export default function Dashboard({orders}: HomeProps){
             </main>   
 
             {modalVisible && (
-                <ModalOrder/>
+                <ModalOrder
+                    isOpen={modalVisible}
+                    onRequestClose={handleCloseModal}
+                    order={modalItem}
+                    handleFinishOrder={ handleFinishItem }
+                />
             )} 
         
         </div>
